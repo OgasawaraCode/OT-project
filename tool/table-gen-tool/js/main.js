696a7820aba8920501c7ca1dc327072f91f5decf
ogasawara-shota <shota2000.0415@gmail.com>
2026-05-01 00:49:56 +0900

    テスト用ダウンロード関数が完成、カレンダー生成関数途中：年、日、曜日を取得

--- tool/table-gen-tool/js/main.js
+++ tool/table-gen-tool/js/main.js
@@ -1,15 +1,36 @@
 // Tampermonkey code
-
 const btn = document.createElement("button");
 btn.innerHTML = "HTML生成ボタン";
 
 document.body.appendChild(btn);
 
-function generateHtml() {
+/**
+ * @param {number} year
+ * @param {number} month
+ */
+
+function handleExport() {
+    generatetable();
+    downloadHtml();
+}
+
+function  generatetable() {
+    const firstDay = new Data(year, month - 1, 1).getDay();
+
+
+
+
+
+
+
+    // const htmlContent = ``;
+}
+
+function downloadHtml() {
     const blob = new Blob(["<h1>hello world</h1>"], { type: 'text/html' });
     const a = Object.assign(document.createElement('a'), { href: URL.createObjectURL(blob), download: 'index.html' });
     a.click();
     URL.revokeObjectURL(a.href);
 }
 
-btn.addEventListener("click", generateHtml);
+btn.addEventListener("click", handleExport);
